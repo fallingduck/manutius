9a10133890502abd5062869ff440875c2748e255
Jack VanDrunen <jvandrun@uci.edu>
2017-03-27 19:12:05 -0700

    Separated server into separate module

--- index.js
+++ index.js
@@ -1,48 +1,27 @@
-var http = require('http')
+var server = require('./lib/server')
 var yaml = require('js-yaml')
 var fs = require('fs')
-
-// Array which stores the routes that the server will check requests against
-var routes = []
-
-// Wrapper function which pushes routes to the array
-var route = function(pattern, f) {
-  routes.push(function(req, res, next) {
-    if (req.url.match(pattern)) {
-      f(req, res, next)
-    } else {
-      next(req, res)
-    }
-  })
-}
 
 
 /* Begin route definitions */
 
-route(/^\/\.git$/, function(req, res) {
+server.route(/^\/\.git$/, function(req, res) {
   res.end('git server')
 })
 
-route(/.*/, function(req, res) {
+server.route(/.*/, function(req, res) {
   res.end('static file server')
 })
 
 /* End route definitions */
 
 
-// Reduce the array of routes into a single router function
-var router = routes.reduceRight(function(b, a, i, z) {
-  return function(req, res) {
-    a(req, res, b)
-  }
-})
-
 // Create the server
 var config = yaml.safeLoad(fs.readFileSync('config.yaml', 'utf8'));
 host = config.host
 port = config.port
 
-var server = http.createServer(router)
+var server = server.makeServer()
 
 server.listen(port, host, function() {
   console.log('Serving on %s:%s...', host, port)
