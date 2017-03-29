00060d9b5ae1e00ac5ccccd152ee3ede2cfc31cb
Jack VanDrunen <jvandrun@uci.edu>
2017-03-28 20:44:26 -0700

    Implemented basic CLI

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-fixpath = require('./lib/fixpath')
-core = require('./lib/core')
+// Start the server
+if (process.argv.length <= 2 || process.argv[2] == 'serve') {
+  var fixpath = require('./lib/fixpath')
+  var core = require('./lib/core')
+  var fs = require('fs')
+  var yaml = require('js-yaml')
 
-fs = require('fs')
-yaml = require('js-yaml')
+  fs.readFile(fixpath('config.yaml'), 'utf8', function(e, out) {
+    if (e) {
+      console.log(e)
+      return
+    }
 
-fs.readFile(fixpath('config.yaml'), 'utf8', function(e, out) {
-  if (e) {
-    console.log(e)
-    return
-  }
-
-  var config = yaml.safeLoad(out)
-  var host = config.host
-  var port = config.port
-  core.runServer(host, port, function() {
-    console.log('Serving on %s:%s...', host, port)
+    var config = yaml.safeLoad(out)
+    var host = config.host
+    var port = config.port
+    core.runServer(host, port, function() {
+      console.log('Serving on %s:%s...', host, port)
+    })
   })
-})
+}
+
+// Print the help message
+else {
+  var package = require('./package.json')
+  console.log('%s v%s', package.name, package.version)
+  console.log(package.description)
+  console.log()
+  console.log('Available commands:')
+  console.log('  serve')
+  console.log('  help')
+}
